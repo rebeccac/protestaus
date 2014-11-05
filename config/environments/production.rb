bc217b030f810b4d1e44931de169fa5994c48975
Rebecca Cordingley <rebecca.j.cordingley@gmail.com>
2014-11-05 22:13:15 +1030

    Production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # Handle Devise mailing functions
-  config.action_mailer.default_url_options = { host: 'protestaus.heroku.com' }
+  config.action_mailer.default_url_options = { host: 'protestaus.herokuapp.com', protocol: 'https' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
 
