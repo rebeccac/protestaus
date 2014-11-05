fd6bc08cc94497d91c884f695acadfcb5a7cfbbd
Rebecca Cordingley <rebecca.j.cordingley@gmail.com>
2014-11-05 22:06:41 +1030

    Devise mailing settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
   # number of complex assets.
   config.assets.debug = true
 
-# Handle Devise mailing functions
+  # Handle Devise mailing functions
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,4 +75,10 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Handle Devise mailing functions
+  config.action_mailer.default_url_options = { host: 'protestaus.heroku.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+
 end
