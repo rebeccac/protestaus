7c104c2aa68b3d4aaf7df992d397dfe7200b3a8a
Rebecca Cordingley <rebecca.j.cordingley@gmail.com>
2014-11-05 21:59:46 +1030

    Devise

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -3,8 +3,8 @@ ActionMailer::Base.delivery_method = :smtp
 		:address				=>	'smtp.sendgrid.net',
 		:port					=>	'587',
 		:authentication			=>	:plain,
-		:user_name				=>	'app28443624@heroku.com',
-		:password				=>	'xv58carw',
+		:user_name				=>	'app31304539@heroku.com',
+		:password				=>	'5xp4uqfc',
 		:domain					=>	'heroku.com',
 		:enable_starttls_auto	=>	true
 	}
