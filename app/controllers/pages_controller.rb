18b7f95aee9a8447f04e2edc42b96042495d1da7
Rebecca Cordingley <rebecca.j.cordingley@gmail.com>
2014-11-06 18:00:28 +1030

    Pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   def index
-   @recent_protests = Protest.all(:order => "id desc", :limit => 6).reverse
+   #@recent_protests = Protest.all(:order => "id desc", :limit => 6).reverse
   end
 
   def related_sites
