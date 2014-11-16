ea7fe38ef958ea443599d5d81acd82f9ddb34fb4
Rebecca Cordingley <rebecca.j.cordingley@gmail.com>
2014-11-16 17:37:04 +1030

    Front page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
   def index
    #@recent_protests = Protest.all(:order => "id desc", :limit => 6).reverse
+   render layout: 'frontpage_layout'
   end
 
   def related_sites
