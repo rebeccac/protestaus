a6befef8ddf8789a3c8a6f426741077ea261cdf8
Rebecca Cordingley <rebecca.j.cordingley@gmail.com>
2014-11-17 11:21:48 +1030

    Front page recent protests

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,8 @@
 class PagesController < ApplicationController
   def index
+   @recent_protests = Protest.visible(params).limit(6).order("id desc")
    render layout: 'frontpage_layout'
-   @recent_protests = Protest.all(:order => "id desc", :limit => 6).reverse
-  end
+   end
 
   def related_sites
   end
