d116b21e0e14eeaa3cea8375932d53b7fcfe0064
Rebecca Cordingley <rebecca.j.cordingley@gmail.com>
2014-11-17 16:06:43 +1030

    Dropdown menu background image test

--- config/application.rb
+++ config/application.rb
@@ -1,10 +1,12 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
+require 'sprockets/railtie'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(*Rails.groups)
+# Bundler.require(*Rails.groups)
+Bundler.require(:default, :assets, Rails.env)
 
 module Pa
   class Application < Rails::Application
