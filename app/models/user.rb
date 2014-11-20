4e69043a41dd27d35beb27cd14fb453a11383183
Rebecca Cordingley <rebecca.j.cordingley@gmail.com>
2014-11-20 18:56:52 +1030

    Devise User forms styling, front page styling

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
    has_many :protests
 
   def my_protests(params) #params will be passed in through controller
-		    protests.paginate(page: params[:page], order: 'created_at DESC', per_page: 10)
+		    protests.order('created_at DESC').paginate(page: params[:page], per_page: 10)
 		    #calling this method on a user object so can go through its questions association - scopes questions returned to only return questions that belong to user method is called on
   end
   def self.all_users(params)
